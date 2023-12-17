82efa52433828e23e59a5d4022b895ada35e03f3
thomasfregene <fregzythomas@gmail.com>
2023-12-17 05:56:02 +0100

    Add project files.

---
+++ Consumer/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//CONSUMER
+
+var factory = new ConnectionFactory { HostName= "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare("simplehashing", "x-consistent-hash");
+
+channel.QueueDeclare("letterbox3");
+channel.QueueDeclare("letterbox4");
+
+//routing key rep the hash space
+channel.QueueBind("letterbox3", "simplehashing", "1");
+channel.QueueBind("letterbox4", "simplehashing", "3");
+
+var consumer1 = new EventingBasicConsumer(channel);
+
+consumer1.Received += (sender, args) =>
+{
+    var body = args.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Queue1 Received message: {message}");
+};
+
+channel.BasicConsume("letterbox3", autoAck: true, consumer: consumer1);
+
+var consumer2 = new EventingBasicConsumer(channel);
+
+consumer2.Received += (sender, args) =>
+{
+    var body = args.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Queue2 Received message: {message}");
+};
+
+channel.BasicConsume("letterbox4", autoAck: true, consumer: consumer2);
+
+Console.WriteLine("Consuming");
+Console.ReadKey();
---
+++ Producer/Program.cs
@@ -0,0 +1,19 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+//PRODUCER
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare("simplehashing", "x-consistent-hash");
+
+var message = "Hash the routing and pass me on please";
+var body = Encoding.UTF8.GetBytes(message);
+
+var routingKey = "Hashxyz me!";
+
+channel.BasicPublish("simplehashing", routingKey, null, body);
+
+Console.WriteLine($"Sending message: {message}");
